af3dfc9fc8cf51ad531f9f05a915ee0d17354f01
Mike Villiger <akirasoft@gmail.com>
2018-09-18 08:54:30 -0500

    unit and build tasks in alpine jdk8 container, implementing gradle cache

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -1,7 +1,13 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
-
+export ROOT_FOLDER=$( pwd )
 export GRADLE_OPTS=-Dorg.gradle.native=false
+
+GRADLE_HOME="${HOME}/.gradle"
+GRADLE_CACHE="${ROOT_FOLDER}/gradle"
+
+[[ -d "${GRADLE_CACHE}" && ! -d "${GRADLE_HOME}" ]] && ln -s "${GRADLE_CACHE}" "${GRADLE_HOME}"
+
 version=`cat version/number`
 cd music-repo
 echo $version
--- ci/tasks/unit.sh
+++ ci/tasks/unit.sh
@@ -1,5 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -e
+export ROOT_FOLDER=$( pwd )
+export GRADLE_OPTS=-Dorg.gradle.native=false
+
+GRADLE_HOME="${HOME}/.gradle"
+GRADLE_CACHE="${ROOT_FOLDER}/gradle"
+
+[[ -d "${GRADLE_CACHE}" && ! -d "${GRADLE_HOME}" ]] && ln -s "${GRADLE_CACHE}" "${GRADLE_HOME}"
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 cd music-repo
